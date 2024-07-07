79ed21278b22774837bdff6cb95bbcd8a23a5d14
StevenSon12 <stivanstf55@gmail.com>
2024-07-07 17:33:53 +0100

    Modified the board state to make some tests

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -35,9 +35,9 @@ class GameBoard
 {
     public int[,] initialBoardState =
     {
-        { 1, 0, 1 },
-        { 1, 0, 1 },
-        { 2, 2, 0 }
+        { 2, 0, 2 },
+        { 1, 1, 2 },
+        { 1, 2, 0 }
     }; 
 
     // prints 2d array from above
